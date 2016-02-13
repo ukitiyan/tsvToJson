c0c1e74e95eb221def51a08bebd3731cb69f2689
ukitiyan <ukitiyan@gmail.com>
2016-02-13 22:46:02 +0900

    first commit

---
+++ tsvToJson.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/csv"
+	"io"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/**
+ * 第1引数：入力ファイル
+ * 第2引数：出力ファイル
+ * 第3引数以降：Json定義（[例]0:key 5:title）
+ */
+func main() {
+
+	var srcFile *os.File
+	if len(os.Args) < 3 {
+		panic("Args error")
+	} else {
+		var err error
+		srcFile, err = os.Open(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+		defer srcFile.Close()
+	}
+	// reader
+	reader := csv.NewReader(srcFile)
+	reader.Comma = '\t'
+	reader.LazyQuotes = true
+
+	var jsondefs []string
+	for i := 3; i < len(os.Args); i++ {
+		jsondefs = append(jsondefs, os.Args[i])
+	}
+
+	var content []byte
+	content = append(content, "[\n"...)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			panic(err)
+		}
+		content = append(content, "{"...)
+		for i := 0; i < len(jsondefs); i++ {
+			var key string
+			var index int
+			key = strings.Split(jsondefs[i], ":")[1]
+			index, _ = strconv.Atoi(strings.Split(jsondefs[i], ":")[0])
+			content = append(content, "\""+key+"\":\""+record[index]+"\""...)
+			if i < len(jsondefs)-1 {
+				content = append(content, ", "...)
+			}
+		}
+		content = append(content, "}\n"...)
+	}
+	ioutil.WriteFile(os.Args[2], content, os.ModePerm)
+}
